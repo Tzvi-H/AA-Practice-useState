700bb860bbaaeef962d3603d6e68fe93dcdbde32
Tzvi H <16guitar@gmail.com>
2022-10-16 22:14:25 +0000

    Update 3 files

--- src/components/UseState/index.js
+++ src/components/UseState/index.js
@@ -1,7 +1,35 @@
+import "./UseState.css";
+import { useState } from "react";
+
 const UseState = () => {
+  const [theme, setTheme] = useState("light");
+  const [count, setCount] = useState(0);
+
   return (
-    <div>
+    <div className={theme}>
       <h1>UseState Component</h1>
+      <button
+        onClick={() => {
+          setTheme("dark");
+        }}
+      >
+        Dark
+      </button>
+      <h1>{count}</h1>
+      <button
+        onClick={() => {
+          setCount((prevCount) => prevCount + 1);
+        }}
+      >
+        Increment
+      </button>
+      <button
+        onClick={() => {
+          setCount((prevCount) => prevCount - 1);
+        }}
+      >
+        Decrement
+      </button>
     </div>
   );
 };
